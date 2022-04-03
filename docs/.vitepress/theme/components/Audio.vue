760971c54e9a126a3fd7cb2a36599b775eed6565
Victor <yomellamovictor@gmail.com>
2022-04-04 01:21:46 +0200

    add audio component

---
+++ docs/.vitepress/theme/components/Audio.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  channelId?: string
+  messageId: string
+}>()
+const channelId = props.channelId ?? 'vitiblog'
+const messageId = props.messageId
+</script>
+
+<template>
+  <ClientOnly>
+    <iframe
+      width="100%"
+      :src="`data:text/html,%3Cbody%3E%3Cscript%20async%3D%22%22%20src%3D%22https%3A%2F%2Ftelegram.org%2Fjs%2Ftelegram-widget.js%3F18%22%20data-telegram-post%3D%22${channelId}%2F${messageId}%22%20data-width%3D%22100%25%22%3E%3C%2Fscript%3E%3C%2Fbody%3E`"
+      frameborder="0"
+    ></iframe>
+  </ClientOnly>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import Tags from './components/Tags.vue'
+import Audio from './components/Audio.vue'
 import Layout from './components/Layout.vue'
 import './custom.css'
 
@@ -8,6 +9,7 @@ export default {
   Layout,
   enhanceApp({ app, router, siteData }) {
     app.component('Tags', Tags)
+    app.component('Audio', Audio)
     // app is the Vue 3 app instance from createApp()
     // router is VitePress' custom router (see `lib/app/router.js`)
     // siteData is a ref of current site-level metadata.
